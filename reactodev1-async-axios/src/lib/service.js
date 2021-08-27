f7ab7bdda45460c27d6c9343afc31dbe7d24673a
ezgidivarci <ezgidivarci1@gmail.com>
2021-08-27 17:58:06 +0300

    react-odev1(async-axios) completed

---
+++ reactodev1-async-axios/src/lib/service.js
@@ -0,0 +1,48 @@
+import fetch from "node-fetch";
+import axios from "axios";
+
+
+// async function getData(user_id){
+
+//     const user = await axios("https://jsonplaceholder.typicode.com/users/"+ user_id);
+
+//     const post = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id);
+//     console.log(user);
+//     console.log(post);
+   // stringify dondu
+// }
+
+// getData(3);
+
+// async function getData(user_id){
+
+//     const { data} = await axios("https://jsonplaceholder.typicode.com/users/"+ user_id); // bu dondu cunku data! 
+
+//     const {post} = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id); //undefined dedi
+//     console.log(data);
+//     console.log(post);
+    
+// }
+
+async function getData(user_id){
+
+    const { data:user } = await axios("https://jsonplaceholder.typicode.com/users/"+ user_id); // bu dondu cunku data! 
+
+    const {data:post} = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id); //undefined dedi
+    console.log(user);
+    console.log(post);
+    
+}
+
+getData(3);
+
+// (async (user_id) => { //data olarak donuyor ismi degistiremeyiz. data: ... boyle yazıcaz
+//     const { data:user } = await axios("https://jsonplaceholder.typicode.com/users/"+ user_id);
+
+//     const { data:post } = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id);
+//     console.log(user);
+//     console.log(post);
+
+// }) (3);
+
+//export default getData;

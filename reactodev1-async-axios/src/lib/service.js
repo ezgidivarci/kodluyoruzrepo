3b93c6499cc70db218b528118dd6b32cffa7b435
ezgidivarci <ezgidivarci1@gmail.com>
2021-08-28 10:06:44 +0300

    Revert "sql-odev3(like-ilike)"
    This reverts commit fed56d2435eb27f95aab9d8c689234c0ab35f560.

--- reactodev1-async-axios/src/lib/service.js
+++ reactodev1-async-axios/src/lib/service.js
@@ -9,7 +9,7 @@ import axios from "axios";
 //     const post = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id);
 //     console.log(user);
 //     console.log(post);
-// stringify dondu
+   // stringify dondu
 // }
 
 // getData(3);
@@ -21,17 +21,17 @@ import axios from "axios";
 //     const {post} = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id); //undefined dedi
 //     console.log(data);
 //     console.log(post);
-
+    
 // }
 
-async function getData(user_id) {
+async function getData(user_id){
 
-    const { data: user } = await axios("https://jsonplaceholder.typicode.com/users/" + user_id);
+    const { data:user } = await axios("https://jsonplaceholder.typicode.com/users/"+ user_id); // bu dondu cunku data! 
 
-    const { data: post } = await axios("https://jsonplaceholder.typicode.com/posts/" + user_id);
+    const {data:post} = await axios("https://jsonplaceholder.typicode.com/posts/"+ user_id); //undefined dedi
     console.log(user);
     console.log(post);
-
+    
 }
 
 getData(3);

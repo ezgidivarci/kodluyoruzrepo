539ada7d80cd16cdcfd97a3fa263a74e4fe945bb
ezgidivarci <ezgidivarci1@gmail.com>
2021-12-03 15:28:34 +0300

    js-odev1

---
+++ js-odev1-clock/clock.js
@@ -0,0 +1,11 @@
+let name = prompt("Lutfen isminizi giriniz. ");
+
+let myName = document.querySelector("#myName");
+myName.innerHTML = `<b>${name}</b>`;
+
+// let text = document.querySelector(".text1");
+// text.innerHTML = `${text.innerHTML} + <b>${myName}</b>`
+
+function showTime() {
+
+}
